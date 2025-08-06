7f31ff76d1333f002f53cb60ad344b365e54922b
LuisAngelX12 <luisangelhm00@gmail.com>
2025-08-05 23:16:23 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -36,10 +36,4 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Usuarios}/{action=Index}/{id?}");
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
-
 app.Run();
