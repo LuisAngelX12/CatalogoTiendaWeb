07560282babf4405965a6299e58366e06110b872
Familia <Familia@PCMIA>
2025-08-06 00:05:59 -0600

    Conexion de Render + PostgreSQL

---
+++ Migrations/20250806060013_InitRender.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogoTiendaWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitRender : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
